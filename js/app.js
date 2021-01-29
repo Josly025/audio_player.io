737403645af5443486eaf35e816ca8b0de32543a
Grantjos12 <grantjoslyn@gmail.com>
2021-01-29 13:55:04 -0600

    audio section complete

--- js/app.js
+++ js/app.js
@@ -3,35 +3,44 @@ const play = document.getElementById("play");
 const pause = document.getElementById("pause");
 const player = document.getElementById("player");
 const searchBtn = document.getElementById("search-btn");
-const artist = search.value;
-console.log(search);
+const audio = document.getElementById("audio");
+const audioSource = document.getElementById("audioSource");
 searchBtn.addEventListener("click", searchTrack);
 
 function searchTrack(e) {
-  console.log(artist);
+  let artist = search.value;
   console.log("hello");
-  e.prevent.default();
+  console.log(artist);
+  const apiCall = {
+    method: "GET",
+    url: "https://deezerdevs-deezer.p.rapidapi.com/search",
+    params: { q: `${artist}` },
+    headers: {
+      "x-rapidapi-key": "cb5b1f2f44msh3ccf1d2e09978fap1363abjsn0c69cbf92586",
+      "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+    },
+  };
+  axios
+    .request(apiCall)
+    .then(function (response) {
+      let tracks = response.data.data;
+      console.log(tracks);
+      audio.setAttribute("src", `${tracks[0].preview}`);
+      let output = ` <h1 class="tracks">${tracks[0].album.title}</h1>
+             <h2 class="tracks">${tracks[0].artist.name}</h2>
+             <img class="uk-border-rounded uk-margin-small-top uk-margin-small-bottom" data-src="${tracks[0].album.cover}" width="auto" height="auto"  uk-img>
+
+             `;
+
+      play.innerHTML = output;
+      playAudio();
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+  e.preventDefault();
 }
 
-// const artist = "eninem";
-// const songs = {
-//   method: "GET",
-//   url: "https://deezerdevs-deezer.p.rapidapi.com/search",
-//   params: { q: `${artist}` },
-//   headers: {
-//     "x-rapidapi-key": "cb5b1f2f44msh3ccf1d2e09978fap1363abjsn0c69cbf92586",
-//     "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-//   },
-// };
-
-// axios
-//   .request(songs)
-//   .then(function (response) {
-//     console.log(response.data.data);
-//   })
-//   .catch(function (error) {
-//     console.error(error);
-//   });
-//  <h2>Song Title</h2>
-//               <h3>Artist</h3>
-//               <!-- <img class="uk-border-rounded" data-src="" data-srcset="" sizes="" width="10rem" height="10rem" alt="" uk-img> -->
+// function playAudio() {
+//   audio.play();
+// }
